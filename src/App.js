12a5d7be13bcf5606e117685a7196b8bed10630c
Thiago B <thiaginho357@gmail.com>
2025-05-21 19:15:24 -0300

    feat: Remover BrowserRouter do componente App e adicionar 404.html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header/Header';
@@ -9,14 +9,12 @@ import PokemonDetails from './pages/Details/PokemonDetails';
 function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter basename="/pokemon-quest">
-        <GlobalStyles />
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pokemon/:name" element={<PokemonDetails />} />
-        </Routes>
-      </BrowserRouter>
+      <GlobalStyles />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokemon/:name" element={<PokemonDetails />} />
+      </Routes>
     </ThemeProvider>
   );
 }
